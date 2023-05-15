e2ad38973594ced56890dbda662a6a22f4066edb
Brijesh Gupta <brijeshngi@gmail.com>
2023-05-15 18:15:32 +0530

    added features

--- models/User.js
+++ models/User.js
@@ -1,26 +1,31 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 const schema = mongoose.Schema({
   name: {
     type: String,
+    required: [true, "name required"],
+    maxlength: [50, "Exceeded number of characters"],
   },
   email: {
     type: String,
+    required: [true, "Email required"],
+    validate: validator.isEmail,
   },
   contact: {
     type: String,
+    required: [true, "contact required"],
   },
   password: {
     type: String,
+    required: [true, "password required"],
   },
   Avatar: {
     public_id: {
       type: String,
-      required: true,
     },
     url: {
       type: String,
-      required: true,
     },
   },
   createdAt: {
